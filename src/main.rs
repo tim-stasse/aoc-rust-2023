763368d2d0a451b81e06f4f05286daabd5a49e85
Tim Stasse <tim.stasse@gmail.com>
2023-12-02 01:52:14 +1100

    Add day 1 puzzle 1 solution

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,51 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::{bail, Context, Result};
+use aoc::day_1_puzzle_1;
+use clap::Parser;
+use log::{debug, error};
+
+#[derive(Parser)]
+struct Cli {
+    /// The advent day
+    #[arg(short, long)]
+    day: u8,
+    /// The advent day
+    #[arg(short, long)]
+    puzzle: u8,
+    /// The path to the input file
+    #[arg(short, long)]
+    input: std::path::PathBuf,
+    /// The verbosity of the log level
+    #[command(flatten)]
+    verbosity: clap_verbosity_flag::Verbosity,
+}
+
+fn main() -> Result<()> {
+    let cli = Cli::parse();
+    env_logger::Builder::new()
+    .filter_level(cli.verbosity.log_level_filter())
+    .init();
+    
+    debug!(
+        "Parsed Cli args: day: `{}`, puzzle: `{}`, input: `{}`, verbosity: `{}`",
+        cli.day,
+        cli.puzzle,
+        cli.input.display(),
+        cli.verbosity.log_level_filter()
+    );
+
+    let input = std::fs::read_to_string(&cli.input)
+        .with_context(|| {
+            error!("Could not read file `{}`", &cli.input.display());
+            format!("could not read file `{}`", &cli.input.display())
+        })?;
+
+    match cli.day {
+        1 => match cli.puzzle {
+            1 => println!("{}", day_1_puzzle_1(input.lines())?),
+            _ => bail!("Unknown puzzle `{}`", cli.puzzle)
+        },
+        _ => bail!("Unknown day `{}`", cli.day)
+    };
+
+    Ok(())
 }
---
+++ tests/day_1.rs
@@ -0,0 +1,23 @@
+use assert_cmd::prelude::*; // Add methods on commands
+use assert_fs::prelude::*; // Create temp files
+use predicates::prelude::*; // Used for writing assertions
+use std::process::Command; // Run programs
+
+#[test]
+fn solve_puzzle_1() -> Result<(), Box<dyn std::error::Error>> {
+    let input_file = assert_fs::NamedTempFile::new("input.txt")?;
+    input_file.write_str("1abc2\npqr3stu8vwx\na1b2c3d4e5f\ntreb7uchet")?;
+
+    let mut cmd = Command::cargo_bin("aoc")?;
+    cmd.args([
+        "--day=1",
+        "--puzzle=1",
+        "--input",
+        input_file.path().to_str().unwrap_or_default(),
+    ]);
+    cmd.assert()
+        .success()
+        .stdout(predicate::str::contains("142"));
+
+    Ok(())
+}
