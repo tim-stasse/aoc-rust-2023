4b030606a912d1e2aea9687a8cf125b789cb9951
Tim Stasse <tim.stasse@gmail.com>
2023-12-02 18:07:15 +1100

    Add integration test for day 1 puzzle 2

--- tests/day_1.rs
+++ tests/day_1.rs
@@ -21,3 +21,22 @@ fn solve_puzzle_1() -> Result<(), Box<dyn std::error::Error>> {
 
     Ok(())
 }
+
+#[test]
+fn solve_puzzle_2() -> Result<(), Box<dyn std::error::Error>> {
+    let input_file = assert_fs::NamedTempFile::new("input.txt")?;
+    input_file.write_str("two1nine\neightwothree\nabcone2threexyz\nxtwone3four\n4nineeightseven2\nzoneight234\n7pqrstsixteen")?;
+
+    let mut cmd = Command::cargo_bin("aoc")?;
+    cmd.args([
+        "--day=1",
+        "--puzzle=2",
+        "--input",
+        input_file.path().to_str().unwrap_or_default(),
+    ]);
+    cmd.assert()
+        .success()
+        .stdout(predicate::str::contains("281"));
+
+    Ok(())
+}
